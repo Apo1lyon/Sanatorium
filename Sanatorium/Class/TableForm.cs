f160c10d2add38e65d552cbc4536c95893a1f33b
Apo1lyon <97633107+Apo1lyon@users.noreply.github.com>
2023-05-29 14:12:04 +0300

    Изменение класса TableForm

--- Sanatorium/Class/TableForm.cs
+++ Sanatorium/Class/TableForm.cs
@@ -10,15 +10,26 @@ using System.Windows.Forms;
 
 namespace Sanatorium.Class
 {
+    /// <summary>
+    /// Класс для работы с формой-таблицей
+    /// </summary>
     public partial class TableForm : Form
     {
         //Свойства
-        private BindingSource BindingSource { get; set; }
         private DataGridView dgvDataBase { get; set; }
         private TextBox TextBoxId { get; set; }
+        /// <summary>
+        /// Название главной таблицы
+        /// </summary>
         public string TablePrimary { get; set; }
-        private Panel Panel { get; set; }
+        private Panel PanelDesktop { get; set; }
+        private Panel PanelSetValue { get; set; }
 
+        /// <summary>
+        /// Загружает данные для формы
+        /// </summary>
+        /// <param name="sender">Отправитель</param>
+        /// <param name="e">Событие</param>
         public void Form_Load(object sender, EventArgs e)
         {
             LoadProperty();
@@ -28,9 +39,10 @@ namespace Sanatorium.Class
 
         private void LoadProperty()
         {
-            foreach (Panel panel in Controls) { if (panel.Name == "panelDesktop") Panel = panel; }
-            foreach (Panel panel in Panel.Controls)
+            foreach (Panel panel in Controls) { if (panel.Name == "panelDesktop") PanelDesktop = panel; }
+            foreach (Panel panel in PanelDesktop.Controls)
             {
+                if (panel.Name == "panelSetValue") PanelSetValue = panel;
                 foreach (Control item in panel.Controls)
                 {
                     if (item is TextBox)
@@ -39,34 +51,41 @@ namespace Sanatorium.Class
                         if (item.Name == "dgvDataBase") dgvDataBase = (DataGridView)item;
                 }
             }
-
         }
         
         private void LoadTheme()
         {
-            foreach (Control panel in Panel.Controls)
+            foreach (Control panel in PanelDesktop.Controls)
             {
                 foreach (Control item in panel.Controls)
                 {
                     if (item is Label) item.ForeColor = ThemeColor.PrimaryColor;
                 }
             }
-        }
+        } //Загружает цветовую тему для формы
         
         private void FillDate()
         {
             BindingSource bindingSourcePrimary = new BindingSource();
             bindingSourcePrimary.DataSource = SqlConnection.GetData($"SELECT * FROM {TablePrimary}", new DataTable($"{TablePrimary}"));
             dgvDataBase.DataSource = bindingSourcePrimary;
-        }
+        } //Загружает данные для таблицы
 
+        /// <summary>
+        /// Обновляет форму-таблицу
+        /// </summary>
         public void UpdateTable()
         {
             FillDate();
-            OperationsDataBase.ClearTextBox(Controls);
+            OperationsDataBase.ClearTextBox(PanelSetValue.Controls);
             TextBoxId.Text = SqlConnection.NextID(dgvDataBase);
         }
 
+        /// <summary>
+        /// Открывает дочернюю форму
+        /// </summary>
+        /// <param name="childForm">Дочерняя форма</param>
+        /// <param name="btnSender">Кнопка отправитель</param>
         public void OpenChildForm(Form childForm, object btnSender)
         {
             Dispose();
@@ -74,12 +93,16 @@ namespace Sanatorium.Class
             childForm.TopLevel = false;
             childForm.FormBorderStyle = FormBorderStyle.None;
             childForm.Dock = DockStyle.Fill;
-            Panel.Controls.Add(childForm);
-            Panel.Tag = childForm;
+            PanelDesktop.Controls.Add(childForm);
+            PanelDesktop.Tag = childForm;
             childForm.BringToFront();
             childForm.Show();
-        }//Открытие дочерней формы
+        }
 
+        /// <summary>
+        /// Добавляет данные в базу данных
+        /// </summary>
+        /// <param name="addQuery">SQL запрос</param>
         public void AddValue(string addQuery)
         {
             try
@@ -87,8 +110,8 @@ namespace Sanatorium.Class
                 SqlConnection.connection.Open();
                 SqlCommand sqlCommand = new SqlCommand(addQuery, SqlConnection.connection);
                 sqlCommand.ExecuteNonQuery();
-                BindingSource = new BindingSource();
-                BindingSource.DataSource = SqlConnection.GetData($"Select * From {TablePrimary}", new DataTable($"{TablePrimary}"));
+                BindingSource bindingSource = new BindingSource();
+                bindingSource.DataSource = SqlConnection.GetData($"Select * From {TablePrimary}", new DataTable($"{TablePrimary}"));
                 UpdateTable();
                 SqlConnection.connection.Close();
             }
@@ -98,6 +121,5 @@ namespace Sanatorium.Class
                 MessageBox.Show($"Некорректные данные или их отсутствие. Проверьте чтобы все данные были введены корректно и не повторялись!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        
     }
 }
